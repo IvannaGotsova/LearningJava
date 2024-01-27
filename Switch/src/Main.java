4c880df1a9b5d5db96ce4e9b8d647fc76f610ff2
IvannaGotsova <ivannagotsova@outlook.com>
2024-01-27 22:28:23 +0200

    Switch statements

---
+++ Switch/src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x = 3;
+		
+		switch (x) {
+		case 1 : System.out.println("You are in first place.");
+		break;
+		case 2 : System.out.println("You are in second place.");
+		break;
+		case 3 : System.out.println("You are in third place.");
+		break;
+		case 4 : System.out.println("You are in fourth place.");
+		break;
+		case 5 : System.out.println("You are in fifth place.");
+		break;
+		default : System.out.println("You are not placed.");
+		}
+	}
+
+}
