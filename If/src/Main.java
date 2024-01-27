48f8f2036de7cb9c0cdd90bd76410088d34eae47
IvannaGotsova <ivannagotsova@outlook.com>
2024-01-27 22:21:24 +0200

    If statements

---
+++ If/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int age = 5;
+		
+		if (age < 18) {
+			System.out.println("You are under 18");
+		} else if (age == 18) {
+			System.out.println("You are exactly 18");
+		} else if (age > 18) {
+			System.out.println("You are over 18");
+		}
+	}
+
+}
