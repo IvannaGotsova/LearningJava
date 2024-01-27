11957b41aba863da78b20e9a170079e1678dbd36
IvannaGotsova <ivannagotsova@outlook.com>
2024-01-27 22:15:59 +0200

    Random numbers

---
+++ RandomNumbers/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Random random = new Random();
+		
+		double x = random.nextDouble(0.1);
+		
+		System.out.println(x);
+	}
+
+}
