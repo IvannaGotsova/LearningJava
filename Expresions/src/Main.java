71cd8a5748357e2d36a9b14635ed1953cb02ee66
IvannaGotsova <ivannagotsova@outlook.com>
2024-01-27 21:19:39 +0200

    Expressions

---
+++ Expresions/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x = 27;
+		
+		int y = x / 2;
+		double z = (double)x / 2;
+		
+		System.out.println(y);
+		System.out.println(z);
+	}
+
+}
