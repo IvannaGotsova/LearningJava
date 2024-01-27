fbd35ddf6c60352d65700d1a9780cdb3a8f6ffc4
IvannaGotsova <ivannagotsova@outlook.com>
2024-01-27 20:56:52 +0200

    Wrap two variables

--- WrapTwoVariables/src/Main.java
+++ WrapTwoVariables/src/Main.java
@@ -3,7 +3,20 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		int x = 1;
+		int y = 2;
+		int temp;
+		
+		System.out.println("X before: " + x);
+		System.out.println("Y before: " + y);
+		
+		temp = x;
+		x = y;
+		y = temp;
+		
+		System.out.println("X after: " + x);
+		System.out.println("Y after: " + y);
 	}
 
 }
