a7e549508e28d8d7471049e135e50ab17927b674
IvannaGotsova <ivannagotsova@outlook.com>
2024-01-28 17:40:43 +0200

    String operations

---
+++ String/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String example = "SomethingForExample";
+		
+		System.out.println("The example is: " + example);
+		System.out.println("The length is: " + example.length());
+		System.out.println("Upper case: " + example.toUpperCase());
+		System.out.println("Lower case: " + example.toLowerCase());
+		System.out.println("First character: " + example.charAt(0));
+		System.out.println("Index of S: " + example.indexOf('S'));
+		System.out.println("Replace S with character ~: " + example.replace('S', '~'));
+		
+	}
+
+}
