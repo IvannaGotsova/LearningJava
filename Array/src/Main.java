3d3c5be14a961bffee6f617d7cb303b185336f3b
IvannaGotsova <ivannagotsova@outlook.com>
2024-01-28 16:35:06 +0200

    Array

---
+++ Array/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int [] numbers = new int [10];
+		
+		for (int i = 0; i < numbers.length; i++) {
+			numbers [i] = i + 1;
+			System.out.println(numbers[i]);
+		}
+	}
+
+}
