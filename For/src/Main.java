4e696eca89dad0d1bfb04f6e61beaee11d63ae21
IvannaGotsova <ivannagotsova@outlook.com>
2024-01-28 16:05:16 +0200

    For

---
+++ For/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x = 0;
+		
+		for (int i = 0; i <= 10; i++) {
+			System.out.println(x);
+			x++; 
+		}
+	}
+
+}
