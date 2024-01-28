3ceb62b1aabdb787fc42c532bb7efc1fa9f15011
IvannaGotsova <ivannagotsova@outlook.com>
2024-01-28 16:10:54 +0200

    Nested Loops

---
+++ NestedLoops/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x = 5; 
+		int y = 10;
+		
+		for (int i = 0; i < x; i++) {
+			System.out.println();
+			for (int j = 0; j < y; j++) {
+				System.out.print("~");
+			}
+		}
+	}
+
+}
