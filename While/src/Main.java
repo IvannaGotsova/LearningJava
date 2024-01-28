e50f70615548d2bf03913526d1f34e9deb90abd8
IvannaGotsova <ivannagotsova@outlook.com>
2024-01-28 16:01:33 +0200

    While

---
+++ While/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x = 0;
+		
+		while (x <= 10) {
+			System.out.println(x);
+			
+			x++;
+		}
+	}
+
+}
