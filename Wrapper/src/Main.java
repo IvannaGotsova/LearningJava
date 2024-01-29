9b3db407236a09dba4b274df09c008f28a3e83e5
IvannaGotsova <ivannagotsova@outlook.com>
2024-01-29 20:58:55 +0200

    Wrapper

---
+++ Wrapper/src/Main.java
@@ -0,0 +1,31 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Integer integerExample = 1;
+		Double doubleExample = 10.5;
+		Character characterExample = 'a';
+		
+		System.out.println(integerExample.doubleValue());
+		System.out.println(integerExample.equals(11));
+		System.out.println(integerExample.toString());
+		System.out.println(integerExample.getClass());
+		System.out.println(integerExample.hashCode());
+		
+		System.out.println(doubleExample.doubleValue());
+		System.out.println(doubleExample.equals(11));
+		System.out.println(doubleExample.toString());
+		System.out.println(doubleExample.getClass());
+		System.out.println(doubleExample.hashCode());
+		
+		System.out.println(characterExample.charValue());
+		System.out.println(characterExample.equals(11));
+		System.out.println(characterExample.toString());
+		System.out.println(characterExample.getClass());
+		System.out.println(characterExample.hashCode());
+		
+	}
+
+}
