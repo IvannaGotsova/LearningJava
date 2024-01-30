e0e7162bf4b1dbc5748a0f64112a92c2fc48bc4b
IvannaGotsova <ivannagotsova@outlook.com>
2024-01-30 21:54:39 +0200

    Find Name

--- ~SimplePrograms/src/Main.java
+++ ~SimplePrograms/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
@@ -8,13 +10,27 @@ public class Main {
 		System.out.println(simpleCalculator(2, 3, '+'));
 		System.out.println(greetMessage("Peter", "Petrov"));
 		loopWithStep(4);
+		System.out.println(findName("Ivan"));
+
 		
+	}
+	
+	static boolean findName(String name) {
+		String [] names = {"Ivan", "Peter", "Dimitar", "Yordan", "Daniel"};
+		List namesList = Arrays.asList(names);
+		
+		if (namesList.contains(name)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 	
 	static void loopWithStep(int x) {
 		for (int i = 0; i < 30; i = i + x) {
 			System.out.print(i + " ");
 		}
+		System.out.println();
 	}
 	
 	static String greetMessage(String first, String last) {
