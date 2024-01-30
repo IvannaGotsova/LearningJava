c7ccfbccb46a74f60889483852f0edfd99d80009
IvannaGotsova <ivannagotsova@outlook.com>
2024-01-30 22:25:42 +0200

    Check day

--- ~SimplePrograms/src/Main.java
+++ ~SimplePrograms/src/Main.java
@@ -12,10 +12,30 @@ public class Main {
 		loopWithStep(4);
 		System.out.println(findName("Ivan"));
 		checkGrade(5.55);
-
+		checkDay("Sunday");
+		
 	}
 	
 
+	static void checkDay(String day) {
+		switch (day) {
+		case "Sunday" : System.out.println("Today, it is Sunday");
+		break;
+		case "Monday" : System.out.println("Today, it is Monday");
+		break;
+		case "Tuesday" : System.out.println("Today, it is Tuesday");
+		break;
+		case "Wednesday" : System.out.println("Today, it is Wednesday");
+		break;
+		case "Thursday" : System.out.println("Today, it is Thursday");
+		break;
+		case "Friday" : System.out.println("Today, it is Friday");
+		break;
+		case "Saturday" : System.out.println("Today, it is Saturday");
+		break;
+		default : System.out.println("Please, insert valid day");
+		}
+	}
 	
 	static void checkGrade (double grade) {
 		if (grade >= 5.50 && grade <= 6) {
