a1ceca804fa93c5bbc7e00a820d37a9c623548e8
IvannaGotsova <ivannagotsova@outlook.com>
2024-01-30 21:39:06 +0200

    Loop with step

--- ~SimplePrograms/src/Main.java
+++ ~SimplePrograms/src/Main.java
@@ -7,6 +7,14 @@ public class Main {
 		System.out.println("Hello World");
 		System.out.println(simpleCalculator(2, 3, '+'));
 		System.out.println(greetMessage("Peter", "Petrov"));
+		loopWithStep(4);
+		
+	}
+	
+	static void loopWithStep(int x) {
+		for (int i = 0; i < 30; i = i + x) {
+			System.out.print(i + " ");
+		}
 	}
 	
 	static String greetMessage(String first, String last) {
