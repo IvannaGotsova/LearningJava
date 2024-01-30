e2ded7bbd5b30be71e41de967d950eba127f3b67
IvannaGotsova <ivannagotsova@outlook.com>
2024-01-30 21:28:52 +0200

    Simple Calculator

--- ~SimplePrograms/src/Main.java
+++ ~SimplePrograms/src/Main.java
@@ -4,6 +4,21 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Hello World");
+		System.out.println(simpleCalculator(2, 3, '+'));
+		
+	}
+	
+	static double simpleCalculator(double first, double second, char x) {
+		if (x == '+') {
+			return first + second;
+		} else if (x == '-') {
+			return first - second;
+		} else if (x == '/') {
+			return first / second;
+		} else {
+			return first * second;
+		} 
 	}
 
 }
