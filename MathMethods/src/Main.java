ca6e4170b6b7e08c372446498829be4ec930ca96
IvannaGotsova <ivannagotsova@outlook.com>
2024-02-02 21:53:39 +0200

    Math Methods

---
+++ MathMethods/src/Main.java
@@ -0,0 +1,51 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x = 10; 
+		int y = 5;
+		int z = 1;
+		double a = -12.754;
+		
+		
+		System.out.println(Math.abs(a));
+		System.out.println(Math.acos(z));
+		System.out.println(Math.asin(z));
+		System.out.println(Math.atan(x));
+		System.out.println(Math.atan2(x, y));
+		System.out.println(Math.cbrt(x));
+		System.out.println(Math.copySign(x, y));
+		System.out.println(Math.cos(x));
+		System.out.println(Math.cosh(x));
+		System.out.println(Math.exp(x));
+		System.out.println(Math.expm1(x));
+		System.out.println(Math.floor(x));
+		System.out.println(Math.getExponent(x));
+		System.out.println(Math.hypot(x, y));
+		System.out.println(Math.IEEEremainder(x, y));
+		System.out.println(Math.log(x));
+		System.out.println(Math.log10(x));
+		System.out.println(Math.log1p(x));
+		System.out.println(Math.max(x, y));
+		System.out.println(Math.min(x, y));
+		System.out.println(Math.nextAfter(x, y));
+		System.out.println(Math.nextUp(x));
+		System.out.println(Math.pow(x, y));
+		System.out.println(Math.random());
+		System.out.println(Math.round(x));
+		System.out.println(Math.rint(x));
+		System.out.println(Math.signum(x));
+		System.out.println(Math.sin(x));
+		System.out.println(Math.sinh(x));
+		System.out.println(Math.sqrt(x));
+		System.out.println(Math.tan(x));
+		System.out.println(Math.tanh(x));
+		System.out.println(Math.toDegrees(x));
+		System.out.println(Math.toRadians(x));
+		System.out.println(Math.ulp(x));
+		
+		
+	}
+}
