ec938049b19a110583c49fd9beb32452862b7eed
IvannaGotsova <ivannagotsova@outlook.com>
2024-02-03 20:05:59 +0200

    Split Array

--- ~SimplePrograms-2/src/Main.java
+++ ~SimplePrograms-2/src/Main.java
@@ -5,10 +5,16 @@ public class Main {
 		// TODO Auto-generated method stub
 
 		System.out.println(sumOfTwoNumbers(5, 7));
+		splitString("Something Anything Nothing Everything");
 	}
 	
 	static int sumOfTwoNumbers (int first, int second) {
 		return first + second;
+	}
+	
+	static void splitString (String string) {
+		String [] stringTwo = string.split(" ");
+		System.out.println(stringTwo[1]);
 	}
 
 }
