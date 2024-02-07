292ca09028b3fff0141874af5b741eeace958ee7
IvannaGotsova <ivannagotsova@outlook.com>
2024-02-07 21:19:36 +0200

    Overload toString method

---
+++ ToString/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Person person = new Person();
+		
+		System.out.println(person);
+	}
+
+}
---
+++ ToString/src/Person.java
@@ -0,0 +1,16 @@
+
+public class Person {
+
+	String firstName = "Ivan";
+	String middleName = "Ivanov";
+	String lastName = "Ivanov";
+	String city = "Sofia";
+	int age = 35;
+	
+	public String toString() {
+		return firstName + " " + middleName + " " + lastName + "\n" + city + "\n" + age;
+	}
+	
+	
+	
+}
