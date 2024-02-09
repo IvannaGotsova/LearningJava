6b8fb4f07822a160ee9c5e49de0693a7a76c69c7
IvannaGotsova <ivannagotsova@outlook.com>
2024-02-10 00:10:34 +0200

    Super keyword

---
+++ SuperKeyword/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Person person = new Person("Dimitar", 55);
+		System.out.println(person.iAmPerson());
+		Student student = new Student("Ivan", 15, 6);
+		System.out.println(student.iAmPerson());
+	}
+
+}
---
+++ SuperKeyword/src/Person.java
@@ -0,0 +1,15 @@
+
+public class Person {
+
+	String name;
+	int age; 
+	
+	Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	String iAmPerson() {
+		return name + " age";
+	}
+}
