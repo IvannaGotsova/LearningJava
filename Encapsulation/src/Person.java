2b3f502da73b1fa5de689a982d359275f562ac19
IvannaGotsova <ivannagotsova@outlook.com>
2024-02-12 22:11:21 +0200

    Encapsulation

---
+++ Encapsulation/src/Person.java
@@ -0,0 +1,17 @@
+
+public class Person {
+
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	private String city;
+	private int age;
+	
+	Person(String firstName, String middleName, String lastName, String city, int age) {
+		this.firstName = firstName;
+		this.middleName = middleName;
+		this.lastName = lastName;
+		this.city = city;
+		this.age = age;
+	}
+}
