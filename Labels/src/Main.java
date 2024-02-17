611abbe13ba4edb52230be6d18b215c61c03a49f
IvannaGotsova <ivannagotsova@outlook.com>
2024-02-18 00:06:50 +0200

    Basic Label Example

---
+++ Labels/src/Main.java
@@ -0,0 +1,23 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		
+		JLabel labelExample = new JLabel();
+		labelExample.setText("TEXT EXAMPLE");
+		
+		JFrame jframeExample = new JFrame();
+
+		jframeExample.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jframeExample.setBounds(250, 250, 250, 250);
+		
+		jframeExample.setVisible(true);
+		jframeExample.add(labelExample);
+	}
+
+}
