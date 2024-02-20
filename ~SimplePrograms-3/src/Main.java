dbfc7481097bb9f51c0900078b704a87a8bdd92e
IvannaGotsova <ivannagotsova@outlook.com>
2024-02-20 21:03:37 +0200

    Creating Frame

---
+++ ~SimplePrograms-3/src/Main.java
@@ -0,0 +1,21 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Create a new JFrame
+        JFrame frameExample = new JFrame("My First Frame Example");
+ 
+        // Set frame properties
+        frameExample.setSize(500, 500); // Set the size of the frame
+ 
+        // Close operation
+        frameExample.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+        // Make the frame visible
+        frameExample.setVisible(true);
+    }
+
+}
