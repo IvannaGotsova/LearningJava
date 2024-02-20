0fb3afe60aea5df3c2f773547dd0932a4dc8c63f
IvannaGotsova <ivannagotsova@outlook.com>
2024-02-20 21:18:58 +0200

    Added more properties of JFrame

--- ~SimplePrograms-3/src/Main.java
+++ ~SimplePrograms-3/src/Main.java
@@ -1,3 +1,6 @@
+import java.awt.Color;
+import java.awt.GridLayout;
+
 import javax.swing.JFrame;
 
 public class Main {
@@ -10,7 +13,10 @@ public class Main {
  
         // Set frame properties
         frameExample.setSize(500, 500); // Set the size of the frame
- 
+        frameExample.setLocationRelativeTo(null); //set your frame on the screen
+        frameExample.getContentPane().setBackground(Color.BLUE); // Set the background to blue
+        frameExample.getContentPane().setBackground(new Color(0, 206, 250)); // Set the background to custom blue color
+         
         // Close operation
         frameExample.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
  
