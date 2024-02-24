d8d214de4bf1c788d99993fce834df3c59a2b32c
IvannaGotsova <ivannagotsova@outlook.com>
2024-02-24 21:09:00 +0200

    JFileChooser

---
+++ SelectFile/src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.JFileChooser;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		JFileChooser jFileChooserExample = new JFileChooser();
+		jFileChooserExample.showSaveDialog(null);
+	}
+
+}
