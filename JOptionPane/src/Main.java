43775de476a54cff2cea28017f15037236af6597
IvannaGotsova <ivannagotsova@outlook.com>
2024-02-24 13:47:57 +0200

    Show Message Dialog

---
+++ JOptionPane/src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		JOptionPane.showMessageDialog(null, "This is error message", "ERROR", JOptionPane.ERROR_MESSAGE);
+		JOptionPane.showMessageDialog(null, "This is information message", "INFORMATION", JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane.showMessageDialog(null, "This is plain message", "PLAIN", JOptionPane.PLAIN_MESSAGE);
+		JOptionPane.showMessageDialog(null, "This is question message", "QUESTION", JOptionPane.QUESTION_MESSAGE);
+		JOptionPane.showMessageDialog(null, "This is warning message", "WARNING", JOptionPane.WARNING_MESSAGE);		
+		
+	}
+
+}
