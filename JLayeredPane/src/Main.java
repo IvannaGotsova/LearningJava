4a7c750b64d9c47dffc950878a07aa780ab9ccb2
IvannaGotsova <ivannagotsova@outlook.com>
2024-02-24 10:46:02 +0200

    JLayeredPane

---
+++ JLayeredPane/src/Main.java
@@ -0,0 +1,44 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		JFrame frameExample = new JFrame();
+		frameExample.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frameExample.setSize(500, 500);
+		frameExample.setLayout(null);
+		
+		JLayeredPane jLayeredPane = new JLayeredPane();
+		jLayeredPane.setBounds(0, 0, 250, 250);
+		
+		JPanel panelExampleOne = new JPanel();
+		panelExampleOne.setBackground(Color.blue);
+		panelExampleOne.setOpaque(true);
+		panelExampleOne.setBounds(50, 50, 100, 100);
+		jLayeredPane.add(panelExampleOne);
+		
+		JPanel panelExampleTwo = new JPanel();
+		panelExampleTwo.setBackground(Color.green);
+		panelExampleTwo.setOpaque(true);
+		panelExampleTwo.setBounds(100, 100, 100, 100);
+		jLayeredPane.add(panelExampleTwo);
+		
+		JPanel panelExampleThree = new JPanel();
+		panelExampleThree.setBackground(Color.red);
+		panelExampleThree.setOpaque(true);
+		panelExampleThree.setBounds(150, 150, 100, 100);
+		jLayeredPane.add(panelExampleThree);
+		
+		frameExample.add(jLayeredPane);
+		frameExample.setVisible(true);
+	}
+
+}
