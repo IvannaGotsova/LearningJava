d6ead6fbdbc0c9403dad9d80a66d820509d291d7
IvannaGotsova <ivannagotsova@outlook.com>
2024-02-24 15:22:43 +0200

    CheckBox with button

--- CheckBox/src/Main.java
+++ CheckBox/src/Main.java
@@ -2,9 +2,13 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 public class Main {
@@ -20,6 +24,19 @@ public class Main {
 		JCheckBox checkBoxExample = new JCheckBox();
 		frameExample.add(checkBoxExample);
 		checkBoxExample.setText("This is Example");
+		
+		JButton buttonExample = new JButton();
+		buttonExample.setText("Right!");
+		frameExample.add(buttonExample);
+		
+		buttonExample.addActionListener((ActionListener) new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if(e.getSource()==buttonExample) {
+                	System.out.println(checkBoxExample.isSelected());
+                }
+                	
+            }
+        });
 		
 		frameExample.pack();
 		frameExample.setVisible(true);
