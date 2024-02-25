648d6bc17ce691337459d6dba2794ab55d7f9618
IvannaGotsova <ivannagotsova@outlook.com>
2024-02-25 11:07:18 +0200

    Variables

--- Variables/src/Main.java
+++ Variables/src/Main.java
@@ -1,27 +1,27 @@
-
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		boolean booleanExample = true;
-		byte byteExample = 100;
-		short shortExample = 10000;
-		int intExample = 1000000000;
-		long longExample = 1000000000;
-		float floatExample = 10.33333f;
-		double doubleExample = 22.3;
-		char charExample = 'a';
-		String stringExample = "Example for string in Java";     
-		
-		System.out.println("My boolean example is: " + booleanExample);
-		System.out.println("My byte example is: " + byteExample);
-		System.out.println("My short example is: " + shortExample);
-		System.out.println("My int example is: " + intExample);
-		System.out.println("My long example is: " + longExample);
-		System.out.println("My float example is: " + floatExample);
-		System.out.println("My double example is: " + doubleExample);
-		System.out.println("My char example is: " + charExample);
-		System.out.println("My string example is: " + stringExample);
-	}
+   public static void main(String[] args) {
+   // TODO Auto-generated method stub
+   
+	   boolean booleanExample = true;
+	   byte byteExample = 100;
+	   short shortExample = 10000;
+	   int intExample = 1000000000;
+	   long longExample = 1000000000;
+	   float floatExample = 10.33333f;
+	   double doubleExample = 22.3;
+	   char charExample = 'a';
+
+	   String stringExample = "Example for string in Java"; 
+	   System.out.println("My boolean example is: " + booleanExample);
+	   System.out.println("My byte example is: " + byteExample);
+	   System.out.println("My short example is: " + shortExample);
+	   System.out.println("My int example is: " + intExample);
+	   System.out.println("My long example is: " + longExample);
+	   System.out.println("My float example is: " + floatExample);
+	   System.out.println("My double example is: " + doubleExample);
+	   System.out.println("My char example is: " + charExample);
+	   System.out.println("My string example is: " + stringExample);
+   }
 
 }
