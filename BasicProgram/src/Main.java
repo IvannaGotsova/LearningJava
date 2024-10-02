a884ee85127bbf91f7834d8bd3c174e9fc5a6200
IvannaGotsova <ivannagotsova@outlook.com>
2024-10-02 23:49:26 +0300

    Basic Program

---
+++ BasicProgram/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int firstNumber;
+		int secondNumber;
+		System.out.println("Enter two numbers:");
+		Scanner scanner = new Scanner(System.in);  
+		firstNumber = scanner.nextInt();   
+		secondNumber = scanner.nextInt();
+		int sum = firstNumber + secondNumber;
+		System.out.println("Sum is: " + sum);
+
+	}
+
+}
