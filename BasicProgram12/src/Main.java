7ce0268e97f6a9098a6e5a0431f16b94d9c05b42
IvannaGotsova <ivannagotsova@outlook.com>
2024-10-03 00:20:52 +0300

    Area of rectangle

---
+++ BasicProgram12/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int length = 5; 
+		int width = 2; 
+		int area = length * width; 
+		System.out.println("Area of rectangle is: " + area); 
+	}
+
+}
