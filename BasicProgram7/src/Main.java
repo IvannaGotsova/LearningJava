e1830b1042d90062f008410ff59c19e7003a113e
IvannaGotsova <ivannagotsova@outlook.com>
2024-10-03 00:28:05 +0300

    Myltiply

---
+++ BasicProgram7/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int firstNumber;
+		int secondNumber;
+		System.out.println("Enter two numbers:");
+		Scanner scanner = new Scanner(System.in);  
+		firstNumber = scanner.nextInt();   
+		secondNumber = scanner.nextInt();
+		int sum = firstNumber * secondNumber;
+		System.out.println("Result is: " + sum);
+	}
+
+}
