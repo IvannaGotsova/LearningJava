f786df7b10742df902d0d5b842b10259edf51488
IvannaGotsova <ivannagotsova@outlook.com>
2024-10-03 00:24:19 +0300

    Hello name

---
+++ BasicProgram2/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String name;
+		System.out.println("Enter your name:");
+		Scanner scanner = new Scanner(System.in);  
+		name = scanner.next();   
+		System.out.println("Hello: " + name);
+	}
+
+}
