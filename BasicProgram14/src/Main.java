2f870f104cf86194cbfbd06413157acf6dff3333
IvannaGotsova <ivannagotsova@outlook.com>
2024-10-03 00:22:48 +0300

    Square

---
+++ BasicProgram14/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int number;
+		System.out.println("Enter your number:");
+		Scanner scanner = new Scanner(System.in);  
+		number = scanner.nextInt();   
+		System.out.println(Math.sqrt(number));
+		
+	}
+
+}
