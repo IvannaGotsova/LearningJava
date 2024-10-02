2078485c00e6d8bfbc25c6a4fae0d0752491182d
IvannaGotsova <ivannagotsova@outlook.com>
2024-10-03 00:18:59 +0300

    Count words

---
+++ BasicProgram10/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 String letters = "dsk ncs ruj rtu tuop oooy uvsg bvnm bdsz xcv bnm";
+		 int countWords = letters.split("\\s").length;
+		 System.out.println(countWords);
+	}
+
+}
