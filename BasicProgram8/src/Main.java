5d104e30243bb5c5ddc89f2395337807d851eef8
IvannaGotsova <ivannagotsova@outlook.com>
2024-10-03 00:29:01 +0300

    Sorted

---
+++ BasicProgram8/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 String[] numbers = {"one", "two", "three", "four", "five"};
+
+		 Arrays.sort(numbers);
+
+		 System.out.println("Sorted numbers: ");
+
+		 for (String number : numbers) {
+
+		 System.out.println(number);
+
+		 }
+	}
+
+}
