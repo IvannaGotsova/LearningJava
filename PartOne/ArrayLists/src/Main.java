7bd134c74f71de2911999491b6e413280a1c976b
IvannaGotsova <ivannagotsova@outlook.com>
2025-07-29 10:21:06 +0300

    ArrayLists

---
+++ PartOne/ArrayLists/src/Main.java
@@ -0,0 +1,21 @@
+package ArrayLists.src;
+
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		
+		
+		for (int i = 0; i < 10; i++) {
+			
+			  numbers.add(i + 1);
+			}
+		
+		System.out.println(numbers.toString());
+	}
+
+}
