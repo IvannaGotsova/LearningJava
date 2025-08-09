e8f79df9812dcb06115f06bc9ecaebb153c45836
IvannaGotsova <ivannagotsova@outlook.com>
2025-08-09 12:24:09 +0300

    Equals

---
+++ PartTwo/Equals/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String stringOne = "Hello";
+		String stringTwo = "Hello";
+		String stringThree = "Hello World!";
+		System.out.println(stringOne.equals(stringTwo)); 
+		System.out.println(stringOne.equals(stringThree)); 
+	}
+
+}
