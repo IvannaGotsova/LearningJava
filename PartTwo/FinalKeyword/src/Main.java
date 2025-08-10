e697ba9e939d29b3da8c584c51453bfb8a8bfd10
IvannaGotsova <ivannagotsova@outlook.com>
2025-08-10 08:47:48 +0300

    Final

---
+++ PartTwo/FinalKeyword/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+    static int staticVariable = 100;
+	final int finalVariable = 100;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println(staticVariable);
+	}
+
+}
