cee4f8a73ee85aa03dc37271183f9a3ede6a24dc
IvannaGotsova <ivannagotsova@outlook.com>
2025-08-10 09:07:26 +0300

    HashCode

---
+++ PartTwo/HashCode/src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String exampleString = "Hello World!";
+		System.out.println(exampleString.hashCode());
+		
+		String exampleStringOne = "Hello";
+		System.out.println(exampleStringOne.hashCode());
+		
+		String exampleStringTwo = "World";
+		System.out.println(exampleStringTwo.hashCode());
+		
+		String exampleStringThree = "!";
+		System.out.println(exampleStringThree.hashCode());
+	}
+
+}
