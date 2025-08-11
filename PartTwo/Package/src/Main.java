5149ecf63ebffeef931db2c89598d40d1ca12522
IvannaGotsova <ivannagotsova@outlook.com>
2025-08-11 13:38:34 +0300

    Package

---
+++ PartTwo/Package/src/Main.java
@@ -0,0 +1,13 @@
+import name.example.Example; 
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Example example = new Example();
+		example.PrintInfo();
+		
+	}
+
+}
---
+++ PartTwo/Package/src/name/example/Example.java
@@ -0,0 +1,14 @@
+package name.example;
+
+public class Example {
+
+	// public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+			public void PrintInfo() {
+				System.out.println("Print from Example");
+			}
+	//}
+
+}
