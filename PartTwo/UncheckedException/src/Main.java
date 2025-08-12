effba8c0ddf83f16bf2f51092e492e8594dee459
IvannaGotsova <ivannagotsova@outlook.com>
2025-08-12 14:14:55 +0300

    Unchecked Exception

---
+++ PartTwo/UncheckedException/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int numberOne = 10;
+		int numberTwo = 0;
+		
+		int result = numberOne / numberTwo;
+		
+		System.out.print(result);
+	}
+
+}
