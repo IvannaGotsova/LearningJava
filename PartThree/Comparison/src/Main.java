1f8b94da2d47167f91d0581047838f9fe4a8519f
IvannaGotsova <ivannagotsova@outlook.com>
2025-08-13 10:31:34 +0300

    Comparison

---
+++ PartThree/Comparison/src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int numberOne = 10;
+		int numberTwo = 10;
+		
+		String stringOne = "aaa";
+		String stringTwo = "aaa";
+		
+		System.out.println(numberOne == numberTwo);
+		System.out.println(stringOne == stringTwo);
+		
+	
+		System.out.println(stringOne.equals(stringTwo));
+		
+	}
+
+}
