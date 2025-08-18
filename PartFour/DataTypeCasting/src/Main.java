526b4100dc9a9ab875a8413ce276b2d4f489d57c
IvannaGotsova <ivannagotsova@outlook.com>
2025-08-18 10:15:09 +0300

    Data Type Casting

---
+++ PartFour/DataTypeCasting/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int intExample = 10;
+		double doubleExample = 20.0d;
+		
+		double intToDouble = intExample;
+		int doubleToInt = (int)doubleExample;
+		
+		System.out.println(intToDouble);
+		System.out.println(doubleToInt);
+		
+	}
+
+}
