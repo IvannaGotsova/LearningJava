a3043740c1847336dab9036e17717222eaf4caeb
IvannaGotsova <ivannagotsova@outlook.com>
2026-03-17 22:41:02 +0200

    Arrays

---
+++ PartSeven/Arrays/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] numbers = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+		
+		for(int i = 0; i < numbers.length; i++) {
+			System.out.println(numbers[i]); 
+			numbers[i] /= 10;
+			System.out.println(numbers[i]); 
+		}
+
+	}
+
+}
