1b5ad43be805b9c8baad44b9252e919b397c564c
IvannaGotsova <ivannagotsova@outlook.com>
2026-03-19 20:57:40 +0200

    ArraysDeques

---
+++ PartSeven/ArrayDeques/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Deque<Integer> arrayDeque = new ArrayDeque<>();
+		
+		for(int i = 1; i <= 10; i++) {
+			arrayDeque.addLast(i);
+		}
+		
+		for (Integer num : arrayDeque) {
+			System.out.println("First number is: " + arrayDeque.peekFirst());
+			System.out.println("Removed: " + arrayDeque.pollFirst());
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			arrayDeque.push(i);
+		}
+		
+		for (Integer num : arrayDeque) {
+			System.out.println("Last number is: " + arrayDeque.peek());
+			System.out.println("Removed: " + arrayDeque.pop());
+		}
+	}
+
+}
