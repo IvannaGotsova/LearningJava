51e14a05cba18550d6289a606d167f9efde4352a
IvannaGotsova <ivannagotsova@outlook.com>
2026-03-31 21:42:40 +0300

    LinkedLists

---
+++ PartSeven/LinkedLists/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		LinkedList<String> numbers = new LinkedList<>();
+		
+		numbers.add("One");
+		numbers.add("Two");
+		numbers.add("Three");
+		numbers.add("Four");
+		numbers.add("Five");
+		numbers.add("Six");
+		numbers.add("Seven");
+		numbers.add("Eight");
+		numbers.add("Nine");
+		numbers.add("Ten");
+		
+		System.out.println(numbers);
+		
+		numbers.addFirst("Zero");
+		numbers.addLast("Eleven");
+		
+		System.out.println(numbers);
+		
+		numbers.removeFirst();
+		numbers.remove("Eleven");
+		
+		System.out.println(numbers);
+	}
+
+}
