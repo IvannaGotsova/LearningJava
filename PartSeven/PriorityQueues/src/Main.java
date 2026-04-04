c8e9ce8c408b55467c3ea08e9dbb07aea4ba031f
IvannaGotsova <ivannagotsova@outlook.com>
2026-04-04 17:37:15 +0300

    PriorityQueues

---
+++ PartSeven/PriorityQueues/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        PriorityQueue<String> numbers = new PriorityQueue<>();
+		
+		numbers.add("One");
+		numbers.add("Two");
+		numbers.add("Three");
+		numbers.add("Four");
+		numbers.add("Five");
+		numbers.add("Six");
+		numbers.add("Seven");
+		numbers.add("Eight");
+		numbers.add("Nine");
+		numbers.add("Ten");
+		
+		System.out.println(numbers);
+		
+		System.out.println(numbers.poll());
+		
+		System.out.println(numbers);
+		
+		System.out.println(numbers.peek());
+	}
+
+}
