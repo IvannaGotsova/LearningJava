3be1228edb393b7323e76084f4759112f4461dd1
IvannaGotsova <ivannagotsova@outlook.com>
2026-04-08 21:29:27 +0300

    Stacks

---
+++ PartSeven/Stacks/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        Stack<String> numbers = new Stack<>();
+		
+		numbers.add("One");
+		numbers.add("Two");
+		numbers.add("Three");
+		numbers.add("Four");
+		numbers.add("Five");
+		numbers.add("Six");
+		numbers.add("Seven");
+		numbers.add("Eight");
+		numbers.add("Nine");
+		numbers.add("Ten");
+		
+		System.out.println(numbers);
+		
+		numbers.push("Eleven");
+		
+		System.out.println(numbers);
+		
+		System.out.println(numbers.peek());
+		
+        numbers.pop();
+		
+		System.out.println(numbers);
+	}
+
+}
