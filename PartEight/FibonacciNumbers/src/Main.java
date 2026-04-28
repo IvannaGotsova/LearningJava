df92ad95f439760b801dad901d18bcdf5b8711c9
IvannaGotsova <ivannagotsova@outlook.com>
2026-04-28 16:05:40 +0300

    FibonacciNumbers

---
+++ PartEight/FibonacciNumbers/src/Main.java
@@ -0,0 +1,31 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		checkFibonacciNumbers(0);
+		
+	}
+	
+	private static void checkFibonacciNumbers(int numbers) {
+		// TODO Auto-generated method stub
+		   int firstNumber = 0;
+		   int secondNumber = 1;
+		   
+		   System.out.print(firstNumber + ", " + secondNumber);
+		   
+		   for (int i = 2; i <= numbers; i++) {
+			   
+			   int nextNumber = firstNumber + secondNumber;
+			   
+			   System.out.print(", " + nextNumber);
+			   
+			   firstNumber = secondNumber;
+			   secondNumber = nextNumber;
+	   
+		   }
+
+	}
+}
