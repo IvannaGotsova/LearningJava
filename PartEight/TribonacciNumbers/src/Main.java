5ce5e8dde9bee733d06e0401ffa7f1eba307e164
IvannaGotsova <ivannagotsova@outlook.com>
2026-04-29 07:12:33 +0300

    addedIfConditions

--- PartEight/TribonacciNumbers/src/Main.java
+++ PartEight/TribonacciNumbers/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		checkTribonacciNumbers(29);
+		checkTribonacciNumbers(0);
 	}
 	
 	private static void checkTribonacciNumbers(int numbers) {
@@ -13,7 +13,9 @@ public class Main {
 		   int secondNumber = 1;
 		   int thirdNumber = 1;
 		   
-		   System.out.print(firstNumber + ", " + secondNumber  + ", " + thirdNumber);
+		   if (numbers == 0) System.out.print(firstNumber);
+		   if (numbers == 1) System.out.print(firstNumber + ", " + secondNumber);
+		   if (numbers >= 2)System.out.print(firstNumber + ", " + secondNumber  + ", " + thirdNumber);
 		   
 		   for (int i = 3; i <= numbers; i++) {
 			   
