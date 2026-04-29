238236cdb7673a8a474e49e6dd1470026ba4595a
IvannaGotsova <ivannagotsova@outlook.com>
2026-04-29 07:11:36 +0300

    addedIfConditions

--- PartEight/FibonacciNumbers/src/Main.java
+++ PartEight/FibonacciNumbers/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 		// TODO Auto-generated method stub
 
 		
-		checkFibonacciNumbers(50);
+		checkFibonacciNumbers(0);
 		
 	}
 	
@@ -14,7 +14,8 @@ public class Main {
 		   int firstNumber = 0;
 		   int secondNumber = 1;
 		   
-		   System.out.print(firstNumber + ", " + secondNumber);
+		   if (numbers == 0) System.out.print(firstNumber);
+		   if (numbers >= 1) System.out.print(firstNumber + ", " + secondNumber);
 		   
 		   for (int i = 2; i <= numbers; i++) {
 			   
