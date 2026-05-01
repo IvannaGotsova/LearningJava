53e7e9ccb1180011f7ee8885ec06b54a5371e720
IvannaGotsova <ivannagotsova@outlook.com>
2026-05-01 19:38:14 +0300

    QuickSort

---
+++ PartEight/QuickSort/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int numbers[] = {5, 2, 1, 87, 9, 64, 53, 34, 32, 33, 90, 100};
+		
+		Arrays.sort(numbers);
+		
+		System.out.println(Arrays.toString(numbers));
+	}
+
+}
