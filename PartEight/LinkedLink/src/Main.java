ead2424e6e4007e3674cbfe38972f0cdc6d8895d
IvannaGotsova <ivannagotsova@outlook.com>
2026-05-05 21:23:11 +0300

    LowestNumber

--- PartEight/LinkedLink/src/Main.java
+++ PartEight/LinkedLink/src/Main.java
@@ -9,8 +9,29 @@ public class Main
         numbers.add(2);
         numbers.addFirst(1);
         numbers.addLast(3);
+        
+        numbers.add(45);
+        numbers.add(12);
+        numbers.add(9);
+        numbers.add(68);
+        numbers.add(25);
+        numbers.add(37);
+        numbers.add(6);
+        numbers.add(17);
+        numbers.add(4);
+        numbers.add(77);
 
-        System.out.println(numbers);
+        int minNumber = numbers.get(0);
+        
+        for (Integer number : numbers) {
+            
+            if (number < minNumber) {
+                
+                minNumber = number;
+            }
+        } 
+        
+        System.out.println("The lowest number is " + minNumber);
         
 	}
 }
