a13666da12fc35c9f8559ee4614ddd3585abddf0
IvannaGotsova <ivannagotsova@outlook.com>
2026-05-07 22:24:22 +0300

    StackFunctions

--- PartEight/Stack/src/Main.java
+++ PartEight/Stack/src/Main.java
@@ -1,9 +1,24 @@
+import java.util.*;
 
-public class Main {
-
+public class Main
+{
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Deque<Integer> numbers = new ArrayDeque<>(List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+		
+		System.out.println(numbers);
+		
+		numbers.push(11);
+		System.out.println(numbers);
+		
+		System.out.println(numbers.pop());
+		numbers.pop();
+		System.out.println(numbers);
+		
+		System.out.println(numbers.peek());
+		
+		System.out.println(numbers.isEmpty());
+		
+		System.out.println(numbers.size());
 	}
-
 }
