509334257d84d29b81995b6e423071dd61825f94
IvannaGotsova <ivannagotsova@outlook.com>
2026-05-07 22:25:05 +0300

    StackAdditional

--- PartEight/Stack/src/Main.java
+++ PartEight/Stack/src/Main.java
@@ -20,5 +20,9 @@ public class Main
 		System.out.println(numbers.isEmpty());
 		
 		System.out.println(numbers.size());
+		
+        System.out.println(numbers.contains(8));
+		numbers.clear();
+		System.out.println(numbers);
 	}
 }
