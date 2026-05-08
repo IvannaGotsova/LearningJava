011448bc2057f694f215d42b9c94683dafe392b6
IvannaGotsova <ivannagotsova@outlook.com>
2026-05-08 22:31:07 +0300

    HashSet

---
+++ PartEight/HashSet/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        Set<Integer> numbers = new HashSet<>(Set.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+		
+		System.out.println(numbers);
+		
+		
+	}
+
+}
