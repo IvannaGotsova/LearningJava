a95ca7f4b951a89960b0fb5651f154e684d75ae0
IvannaGotsova <ivannagotsova@outlook.com>
2026-05-08 22:35:40 +0300

    HashTable

---
+++ PartEight/HashTable/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Hashtable<Integer, String> numbers = new Hashtable<>(Map.of(
+	            1, "One",
+	            2, "Two",
+	            3, "Three", 
+	            4, "Four", 
+	            5, "Five"
+	        ));
+			
+			System.out.println(numbers);
+	}
+
+}
