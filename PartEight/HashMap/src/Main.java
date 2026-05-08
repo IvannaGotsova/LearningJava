ac6673058d4bf881537a296a7b2239cc937fc5b0
IvannaGotsova <ivannagotsova@outlook.com>
2026-05-08 22:25:58 +0300

    HashMap

---
+++ PartEight/HashMap/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        Map<Integer, Integer> numbers = Map.of(0, 10, 1, 11, 2, 12, 3, 13, 4, 14, 5, 15);
+		
+		System.out.println(numbers);
+	}
+
+}
