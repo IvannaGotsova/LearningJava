3da247bedad5a62766fce584a6d20d0f3f876d87
IvannaGotsova <ivannagotsova@outlook.com>
2026-05-08 22:27:27 +0300

    HashMap

--- PartEight/HashMap/src/Main.java
+++ PartEight/HashMap/src/Main.java
@@ -5,9 +5,23 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-        Map<Integer, Integer> numbers = Map.of(0, 10, 1, 11, 2, 12, 3, 13, 4, 14, 5, 15);
+        Map<Integer, Integer> numbers = new HashMap<>(Map.of(0, 10, 1, 11, 2, 12, 3, 13, 4, 14, 5, 15));
 		
 		System.out.println(numbers);
+		
+		numbers.put(0, 00);
+		System.out.println(numbers);
+		
+		numbers.put(0, 10);
+		System.out.println(numbers);
+		
+		System.out.println(numbers.get(0));
+		
+		numbers.remove(0, 11);
+		System.out.println(numbers);
+		
+		System.out.println(numbers.containsKey(0));
+		System.out.println(numbers.containsValue(0));
 	}
 
 }
