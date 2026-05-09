2628e060b57c24725d587c05de5c68f5b6903d0c
IvannaGotsova <ivannagotsova@outlook.com>
2026-05-09 21:15:39 +0300

    BinarySearchTime

---
+++ PartEight/BinarySearchTime/src/Main.java
@@ -0,0 +1,50 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        long startTime = System.nanoTime();
+		
+		int[] initialNumbers = {17, 18, 55, 90, 420, 3800, 20000, 66666, 700000, 10000000};
+	    
+	    int targetNumber = 55;
+	    boolean foundNumber = false;
+	    
+	    int start = 0;
+        int stop = initialNumbers.length - 1;
+
+        while (start <= stop) {
+            
+            int middle = start + (stop - start) / 2;
+
+            if (initialNumbers[middle] == targetNumber) {
+                
+                System.out.println(targetNumber + " is found");
+                foundNumber = true;
+                break;
+                
+            } else if (initialNumbers[middle] < targetNumber) {
+                
+                start = middle + 1; 
+                
+            } else {
+                
+                stop = middle - 1; 
+            }
+        }
+        
+        if (!foundNumber) {
+            
+            System.out.println(targetNumber + " is not found");
+        }
+        
+        long endTime = System.nanoTime();
+
+        long duration = (endTime - startTime);
+        
+        double milliseconds = duration / 1_000_000.0;
+
+        System.out.println(duration + " ns");
+	}
+}
