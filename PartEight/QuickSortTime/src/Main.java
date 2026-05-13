bff9d34df106bb8537a8e69c81432f54346fcd0a
IvannaGotsova <ivannagotsova@outlook.com>
2026-05-13 20:39:17 +0300

    Sync

--- PartEight/QuickSortTime/src/Main.java
+++ PartEight/QuickSortTime/src/Main.java
@@ -1,9 +1,24 @@
+import java.util.Arrays;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+        long startTime = System.nanoTime();
+		
+		int numbers[] = {5, 2, 1, 87, 9, 64, 53, 34, 32, 33, 90, 100};
+		
+		Arrays.sort(numbers);
+		
+		System.out.println(Arrays.toString(numbers));
+        
+        long endTime = System.nanoTime();
 
+        long duration = (endTime - startTime);
+        
+        double milliseconds = duration / 1_000_000.0;
+
+        System.out.println(duration + " ns");
+	}
 }
--- PartEight/SelectionSortTime/src/Main.java
+++ PartEight/SelectionSortTime/src/Main.java
@@ -1,9 +1,39 @@
+import java.util.Arrays;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+long startTime = System.nanoTime();
+		
+		int numbers[] = { 4, 3, 5, 12, 7, 1, 9 };
+		
+        for (int i = 0; i < numbers.length - 1; i++) {
+        	
+        	int minNumber = i;
+        	
+            for (int j = i + 1; j < numbers.length; j++) {
+            	
+            	if (numbers[j] < numbers[minNumber])
+                    minNumber = j;   
+         
+              }
+            
+            int temp = numbers[minNumber];
+            numbers[minNumber] = numbers[i];
+            numbers[i] = temp;
+            
+        }
+        
+        System.out.println(Arrays.toString(numbers));
+        
+        long endTime = System.nanoTime();
 
+        long duration = (endTime - startTime);
+        
+        double milliseconds = duration / 1_000_000.0;
+
+        System.out.println(duration + " ns");
+	}
 }
