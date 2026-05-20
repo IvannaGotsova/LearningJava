07984d5bca2922c634dc8411d8b484c38538201a
IvannaGotsova <ivannagotsova@outlook.com>
2026-05-20 21:08:07 +0300

    checkMethod

--- PartNine/EuclideanAlgorithm/src/Main.java
+++ PartNine/EuclideanAlgorithm/src/Main.java
@@ -4,13 +4,20 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int numberOne = 24, numberTwo = 12;
+
+		int result = euclideanAlgorithm(numberOne, numberTwo);
+
+		System.out.println(result);
 	}
 	
 	public static int euclideanAlgorithm(int numberOne, int numberTwo) {
 
 		if (numberTwo == 0) {
+			
 			return Math.abs(numberOne);
 		}
+		
 		return euclideanAlgorithm(numberTwo, numberOne % numberTwo);
 	}
 
