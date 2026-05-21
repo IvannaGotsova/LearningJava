6c8b1b9e1fc8ddd6370c673e9d39aa34dde77dc9
IvannaGotsova <ivannagotsova@outlook.com>
2026-05-21 20:49:01 +0300

    LazyAlgorithmClass

--- PartNine/LazyAlgorithm/src/Main.java
+++ PartNine/LazyAlgorithm/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Main {
 
@@ -5,5 +8,49 @@ public class Main {
 		// TODO Auto-generated method stub
 
 	}
+	
+	public class ShoppingList {
+		
+	    private String shoppingListTitle;
+	    
+	    private List<String> shoppingListItems = null; 
+	    
+	    public ShoppingList(String shoppingListTitle) {
+	    	
+	        this.shoppingListTitle = shoppingListTitle;
+	    }
 
+	    private List<String> getItemsShoppingList() {
+	    	
+	        System.out.println("Reading shopping list items");
+	        
+	        List<String> shoppingListItems = new ArrayList<>();
+	        
+	        shoppingListItems.add("Notebook");
+	        shoppingListItems.add("Pens");
+	        shoppingListItems.add("Sketchpad");
+	        shoppingListItems.add("Pencils");
+	        shoppingListItems.add("Pen");
+	        shoppingListItems.add("Pencil");
+	        shoppingListItems.add("Book");
+	        shoppingListItems.add("Paintings");
+	        shoppingListItems.add("Gray pencil");
+	        shoppingListItems.add("Black pen");
+	        
+	        return shoppingListItems;
+	    }
+
+	    public List<String> getShoppingListItems() {
+	        
+	    	if (shoppingListItems == null) {
+	    		
+	    		shoppingListItems = getShoppingListItems();
+	        }
+	        return shoppingListItems;
+	    }
+	}
 }
+
+
+
+
