aceee507a16b81803a0c6d7e993e9619a8a47780
IvannaGotsova <ivannagotsova@outlook.com>
2026-05-23 16:10:26 +0300

    CreatingObjects

--- PartNine/EagerAlgorithm/src/Main.java
+++ PartNine/EagerAlgorithm/src/Main.java
@@ -4,6 +4,11 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+        ShoppingList shoppingList = new ShoppingList("Booktore");
+        
+        System.out.println("Created Shopping List");
+        
+        
 	}
 
 }
