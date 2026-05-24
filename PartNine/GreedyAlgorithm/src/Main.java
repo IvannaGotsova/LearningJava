a33025b23142f28fef425192a171dd66bca20139
IvannaGotsova <ivannagotsova@outlook.com>
2026-05-24 17:16:11 +0300

    mainClass

--- PartNine/GreedyAlgorithm/src/Main.java
+++ PartNine/GreedyAlgorithm/src/Main.java
@@ -4,6 +4,11 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int request = 240;
+		
+		GreadyAlgorithm greedyAlgorithm = new GreadyAlgorithm();
+		
+		greedyAlgorithm.GreedyATM(request);
 	}
 
 }
