adc2379c346b6492e728ea8fabaa92ed810aa22e
IvannaGotsova <ivannagotsova@outlook.com>
2026-05-26 20:39:14 +0300

    ChangeInteger

--- PartNine/Factorial/src/Main.java
+++ PartNine/Factorial/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		 int number = 0;
+		 int number = 5;
 		    
 		    if (number < 0) {
 		        
