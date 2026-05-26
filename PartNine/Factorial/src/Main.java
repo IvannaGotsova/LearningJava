0e15ed2ad338e2119ee7ba4db38abb6d4f6ad2cb
IvannaGotsova <ivannagotsova@outlook.com>
2026-05-26 20:38:22 +0300

    Logic

--- PartNine/Factorial/src/Main.java
+++ PartNine/Factorial/src/Main.java
@@ -1,9 +1,26 @@
+import java.math.BigInteger;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		 int number = 0;
+		    
+		    if (number < 0) {
+		        
+	            throw new IllegalArgumentException("Number must be bigger or equal to 0.");
+	        }
+			
+			BigInteger result = BigInteger.ONE;
+			
+	        for (int i = 2; i <= number; i++) {
+	            
+	            result = result.multiply(BigInteger.valueOf(i));
+	        }
+	        
+	        System.out.println(result); 
+	        
 	}
 
 }
