303da2b88711affd7e6617bd8e4ec31738840207
IvannaGotsova <ivannagotsova@outlook.com>
2026-06-03 19:56:42 +0300

    RecursiveFactorial

--- PartNine/RecursiveFactorial/src/Main.java
+++ PartNine/RecursiveFactorial/src/Main.java
@@ -5,5 +5,20 @@ public class Main {
 		// TODO Auto-generated method stub
 
 	}
+	
+    public static long calculateRecursiveFactorial(int number) {
+	    
+        if (number < 0 || number > 20) {
+            
+            throw new IllegalArgumentException("Number must be between 0 and 20.");
+        }
+        
+        if (number == 0 || number == 1) { 
+            
+            return 1;
+        }
+
+        return number * calculateRecursiveFactorial(number - 1); 
+    }
 
 }
