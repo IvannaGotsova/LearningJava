e5850fb0609f793dbd8e32dd9319982a63f561e5
IvannaGotsova <ivannagotsova@outlook.com>
2026-06-10 20:51:47 +0300

    Palindrome

--- PartNine/PalindromeIntegers/src/Main.java
+++ PartNine/PalindromeIntegers/src/Main.java
@@ -3,7 +3,26 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		   String word = "word";
+		    
 
+	        int left = 0;
+	        int right = word.length() - 1;
+	        
+	        while (left < right) {
+	            
+	            if (word.charAt(left) != word.charAt(right)) {
+	                
+	                System.out.println("No Palindrome");
+	                return;
+	            }
+	            
+	            left++;
+	            right--;
+	        }
+	        
+
+		}
+		
 	}
-
-}
