c162dacf1df5834dcd50677d7955cc766455adf1
IvannaGotsova <ivannagotsova@outlook.com>
2026-06-13 15:49:48 +0300

    CorrectedFile

--- PartNine/PalindromeIntegers/src/Main.java
+++ PartNine/PalindromeIntegers/src/Main.java
@@ -4,29 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		   String word = "word";
-		    
-	        if (word == null) {
-	            
-	            System.out.println("Input must be string!");
-	        }
-	        
-	        int left = 0;
-	        int right = word.length() - 1;
-	        
-	        while (left < right) {
-	            
-	            if (word.charAt(left) != word.charAt(right)) {
-	                
-	                System.out.println("No Palindrome");
-	                return;
-	            }
-	            
-	            left++;
-	            right--;
-	        }
-	        
-	        System.out.println("Palindrome");
+		   
 		}
 		
 	}
