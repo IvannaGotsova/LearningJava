91487cd37190c7a4911919ff00c94bd40adf83b3
IvannaGotsova <ivannagotsova@outlook.com>
2026-06-14 14:11:45 +0300

    Palindrome

--- PartNine/PalindromeIntegers/src/Main.java
+++ PartNine/PalindromeIntegers/src/Main.java
@@ -1,10 +1,34 @@
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		   
-		}
-		
+public static void main(String[] args) {
+	    
+	    int number = 854458;
+	    
+        if (number < 0) {
+            
+            System.out.println("Input must be bigger than 0!");
+        }
+        
+        int original = number;
+        int reversed = 0;
+        
+         while (number != 0) {
+             
+            int digit = number % 10;          
+            reversed = reversed * 10 + digit; 
+            number /= 10;                     
+        }
+        
+        if (original == reversed ) {
+            
+            System.out.println("Palindrome");
+        } 
+        else {
+            
+            System.out.println("No Palindrome");
+        }
+
 	}
+	
+}
